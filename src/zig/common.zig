bfd99484b9803c0b38d86b6190ec474ad23ce2d3
bluesillybeard <bluesillybeard2@gmail.com>
2024-06-16 23:30:17 -0600

    Start refactoring

---
+++ src/zig/common.zig
@@ -0,0 +1,23 @@
+// Common for all platforms.
+// The individual platform file imports this file.
+
+const c = @cImport({
+    @cInclude("pinc.h");
+    @cInclude("pincinternal.h");
+});
+
+pub fn Common(NativeWindow: type) type {
+    return struct {
+        pub const PincWindow = struct {
+            native: NativeWindow,
+            // per-window event buffers
+            eventWindowResize: ?c.pinc_event_window_resize_t = null,
+            eventWindowFocus: ?c.pinc_event_window_focus_t = null,
+            eventWindowUnfocus: ?c.pinc_event_window_unfocus_t = null,
+            eventWindowDamaged: ?c.pinc_event_window_damaged_t = null,
+            eventWindowCursorMove: ?c.pinc_event_window_cursor_move_t = null,
+            eventWindowScroll: ?c.pinc_event_window_scroll_t = null,
+            eventWindowClose: ?c.pinc_event_window_close_t = null,
+        };
+    };
+}
