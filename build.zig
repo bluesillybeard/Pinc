c0b1f498bb48a7ebc3fde6f65e3f5a6fdb7fe9da
bluesillybeard <bluesillybeard2@gmail.com>
2024-06-18 00:57:26 -0600

    get working on both stable and master

--- build.zig
+++ build.zig
@@ -5,39 +5,39 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const pincStatic = b.addStaticLibrary(.{
-        .root_source_file = .{ .path = "src/zig/pinc.zig" },
+        .root_source_file = b.path("src/zig/pinc.zig"),
         .name = "pinc",
         .optimize = optimize,
         .target = target,
         .link_libc = true,
     });
-    pincStatic.addIncludePath(.{ .path = "include" });
-    pincStatic.addIncludePath(.{ .path = "ext" });
-    pincStatic.addIncludePath(.{ .path = "src/c" });
+    pincStatic.addIncludePath(b.path("include"));
+    pincStatic.addIncludePath(b.path("ext"));
+    pincStatic.addIncludePath(b.path("src/c"));
     pincStatic.addCSourceFiles(.{
         .files = &[_][]const u8{
             "pincx.c",
             "pincwin32.c",
         },
-        .root = .{ .path = "src/c" },
+        .root = b.path("src/c"),
     });
 
     const pincDynamic = b.addSharedLibrary(.{
-        .root_source_file = .{ .path = "src/zig/pinc.zig" },
+        .root_source_file = b.path("src/zig/pinc.zig"),
         .name = "pinc",
         .optimize = optimize,
         .target = target,
         .link_libc = true,
     });
-    pincDynamic.addIncludePath(.{ .path = "include" });
-    pincDynamic.addIncludePath(.{ .path = "ext" });
-    pincDynamic.addIncludePath(.{ .path = "src/c" });
+    pincDynamic.addIncludePath(b.path("include"));
+    pincDynamic.addIncludePath(b.path("ext"));
+    pincDynamic.addIncludePath(b.path("src/c"));
     pincDynamic.addCSourceFiles(.{
         .files = &[_][]const u8{
             "pincx.c",
             "pincwin32.c",
         },
-        .root = .{ .path = "src/c" },
+        .root = b.path("src/c"),
     });
 
     // This is the basic window example
@@ -46,9 +46,9 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .name = "window",
     });
-    exe.addIncludePath(.{ .path = "include" });
+    exe.addIncludePath(b.path("include"));
     exe.addCSourceFile(.{
-        .file = .{ .path = "examples/window.c" },
+        .file = b.path("examples/window.c"),
     });
     exe.linkLibrary(pincStatic);
     exe.step.dependOn(&pincStatic.step);
@@ -69,7 +69,7 @@ pub fn build(b: *std.Build) void {
     var dynamicStep = b.step("dynamic", "Build Pinc as a dynaic / shader library (.so / .dll)");
     dynamicStep.dependOn(&dynamicInstall.step);
 
-    const headerInstall = b.addInstallHeaderFile(.{ .path = "include/pinc.h" }, "pinc.h");
+    const headerInstall = b.addInstallHeaderFile(b.path("include/pinc.h"), "pinc.h");
     staticStep.dependOn(&headerInstall.step);
     dynamicStep.dependOn(&headerInstall.step);
 }
