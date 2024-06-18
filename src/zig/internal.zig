85e9a1afc8669e5001c6dfc7942b0616d65faf0f
bluesillybeard <bluesillybeard2@gmail.com>
2024-06-18 00:10:54 -0600

    More refactoring

---
+++ src/zig/internal.zig
@@ -0,0 +1,34 @@
+// Internal stuff.
+// This is the implementation of pincinternal.c
+
+const c = @import("c.zig");
+const pinc = @import("pinc.zig");
+const std = @import("std");
+
+pub export fn pinci_make_error(er: c.pinc_error_enum, str: [*:0]const u8) bool {
+    pinc.latestError = er;
+    pinc.latestErrorString = str;
+    return false;
+}
+
+pub export fn pinci_alloc_string(length: usize) [*:0]u8 {
+    // For now, just use the ordinary allocator
+    // TODO: more efficient string allocator
+    const buffer = pinc.allocator.alloc(u8, length + 1) catch std.debug.panic("Failed to allocate string", .{});
+    return @ptrCast(buffer.ptr);
+}
+
+pub export fn pinci_dupe_string(str: [*:0]u8) [*:0]u8 {
+    const strSl = std.mem.sliceTo(str, 0);
+    const buffer = pinc.allocator.dupeZ(u8, strSl) catch std.debug.panic("Failed to allocate memory", .{});
+    return buffer.ptr;
+}
+
+pub export fn pinci_free_string(str: [*:0]u8) void {
+    const buffer: []u8 = std.mem.sliceTo(str, 0);
+    pinc.allocator.free(buffer);
+}
+
+pub export fn pinci_send_event(event: c.pinc_event_union_t) void {
+    pinc.pinci_send_event(event);
+}
--- src/zig/x11.zig
+++ src/zig/x11.zig
@@ -6,15 +6,13 @@ const c = @import("c.zig");
 
 const std = @import("std");
 
-const Common = @import("common.zig").Common(c.x11_window);
-
-const PincWindow = Common.PincWindow;
+const pinc = @import("pinc.zig");
 
 // x11 -> used only by the x11 backend
 
-fn x11_get_window_handle(xid: u32) callconv(.C) c.pinc_window_incomplete_handle_t {
+export fn x11_get_window_handle(xid: u32) c.pinc_window_incomplete_handle_t {
     // xid is the Xlib ID of the window
-    for (Common.windows.items, 0..) |windowOrNone, i| {
+    for (pinc.windows.items, 0..) |windowOrNone, i| {
         if (windowOrNone) |window| {
             if (window.native.xWindow == xid) return @intCast(i + 1);
         }
@@ -24,26 +22,13 @@ fn x11_get_window_handle(xid: u32) callconv(.C) c.pinc_window_incomplete_handle_
     return 0;
 }
 
-fn x11_get_x_window(window: c.pinc_window_incomplete_handle_t) callconv(.C) ?*c.x11_window {
+export fn x11_get_x_window(window: c.pinc_window_incomplete_handle_t) callconv(.C) ?*c.x11_window {
     // TODO: on the C side, check for null
     if (window == 0) return null;
-    if (window - 1 >= Common.windows.items.len) return null;
-    if (Common.windows.items[window - 1] == null) return null;
-    return &Common.windows.items[window - 1].?.native;
+    if (window - 1 >= pinc.windows.items.len) return null;
+    if (pinc.windows.items[window - 1] == null) return null;
+    return &pinc.windows.items[window - 1].?.native;
 }
-
-// We do it this way so functions are only expose to Pinc internals.
-// We don't want users trying to use these - instead they should submit an issue with their use case and why they need the function.
-// (and ideally a poll request adding a proper feature)
-comptime {
-    @export(x11_get_window_handle, .{ .name = "x11_get_window_handle", .linkage = std.builtin.GlobalLinkage.link_once });
-    @export(x11_get_x_window, .{ .name = "x11_get_x_window", .linkage = std.builtin.GlobalLinkage.link_once });
-    @export(Common.pinci_send_event, .{ .name = "pinci_send_event", .linkage = std.builtin.GlobalLinkage.link_once });
-}
-
-const AllocatorType = std.heap.GeneralPurposeAllocator(.{});
-var allocatorObj: AllocatorType = undefined;
-var allocator: std.mem.Allocator = undefined;
 
 // the C api cannot use Zig errors,
 // and returning a result type on every single function is just really annoying.
@@ -51,46 +36,13 @@ var allocator: std.mem.Allocator = undefined;
 var latestError: c.pinc_error_enum = c.pinc_error_none;
 var latestErrorString: [*:0]const u8 = "";
 
-// ALL public pinc functions are defined here.
-// They may simply call into another function from another file, but they are all in this one place so it's easy to find them.
-
 pub export fn pinc_init(window_api: c.pinc_window_api_enum, graphics_api: c.pinc_graphics_api_enum) bool {
-    allocatorObj = .{};
-    allocator = allocatorObj.allocator();
-    // window API specific things
-    var actualWindowAPI = window_api;
-    if (window_api == c.pinc_window_api_automatic) {
-        actualWindowAPI = c.pinc_window_api_x;
-    }
-    return Common.pinc_common_init(actualWindowAPI, graphics_api, allocator, c.x11_init);
+    _ = window_api;
+    return pinc.init(graphics_api, c.x11_init);
 }
 
 pub export fn pinc_destroy() void {
     c.x11_deinit();
-}
-
-pub export fn pinci_make_error(er: c.pinc_error_enum, str: [*:0]const u8) bool {
-    latestError = er;
-    latestErrorString = str;
-    return false;
-}
-
-pub export fn pinci_alloc_string(length: usize) [*:0]u8 {
-    // For now, just use the ordinary allocator
-    // TODO: more efficient string allocator
-    const buffer = allocator.alloc(u8, length + 1) catch std.debug.panic("Failed to allocate string", .{});
-    return @ptrCast(buffer.ptr);
-}
-
-pub export fn pinci_dupe_string(str: [*:0]u8) [*:0]u8 {
-    const strSl = std.mem.sliceTo(str, 0);
-    const buffer = allocator.dupeZ(u8, strSl) catch std.debug.panic("Failed to allocate memory", .{});
-    return buffer.ptr;
-}
-
-pub export fn pinci_free_string(str: [*:0]u8) void {
-    const buffer: []u8 = std.mem.sliceTo(str, 0);
-    allocator.free(buffer);
 }
 
 pub export fn pinc_error_get() c.pinc_error_enum {
@@ -102,29 +54,25 @@ pub export fn pinc_error_string() [*:0]const u8 {
 }
 
 pub export fn pinc_get_window_api() c.pinc_window_api_enum {
-    // X11 is the only supported API at the moment.
-    // In the future, this will require more complex logic as
-    // 1: the API depends on the system and
-    // 2: on Posix systems, it may be determined at runtime to be either X11 or Wayland
     return c.pinc_window_api_x;
 }
 pub export fn pinc_window_incomplete_create(title: [*:0]u8) c.pinc_window_incomplete_handle_t {
     const xWindow = c.x11_window_incomplete_create(title);
-    const windowObj = PincWindow{ .native = xWindow };
+    const windowObj = pinc.Window{ .native = xWindow };
     // TODO: find an empty spot
-    Common.windows.append(windowObj) catch {
-        _ = pinci_make_error(c.pinc_error_allocation, "Failed to create window: allocation failed");
+    pinc.windows.append(windowObj) catch {
+        _ = c.pinci_make_error(c.pinc_error_allocation, "Failed to create window: allocation failed");
         return 0;
     };
     // This is here because LLDB is freaking stupid and can't find any static Zig variables
-    const wins = &Common.windows;
+    const wins = &pinc.windows;
     return @intCast(wins.items.len);
 }
 pub export fn pinc_window_set_size(window: c.pinc_window_incomplete_handle_t, width: u16, height: u16) bool {
     // If someone can get a window more than 32k pixels in size and still be practical, I'll be quite impressed
     // In fact, after 32767 or so, Mutter or X.org (not sure which one causes it) breaks down and stops rendering the window.
     if ((width > 32767) or (height > 32767) or (width == 0) or (height == 0)) {
-        _ = pinci_make_error(c.pinc_error_some, "Could not set window size: Width or Height out of range");
+        _ = c.pinci_make_error(c.pinc_error_some, "Could not set window size: Width or Height out of range");
         return false;
     } else {
         c.x11_set_window_size(window, width, height);
@@ -133,27 +81,27 @@ pub export fn pinc_window_set_size(window: c.pinc_window_incomplete_handle_t, wi
 }
 pub export fn pinc_window_get_width(window: c.pinc_window_incomplete_handle_t) u16 {
     if (window == 0) {
-        _ = pinci_make_error(c.pinc_error_null_handle, "pinc_window_get_width was given a null window");
+        _ = c.pinci_make_error(c.pinc_error_null_handle, "pinc_window_get_width was given a null window");
         return 0;
     }
     const xWindowOrNone = x11_get_x_window(window);
     if (xWindowOrNone) |xWindow| {
         return @intCast(xWindow.width);
     } else {
-        _ = pinci_make_error(c.pinc_error_null_handle, "pinc_window_get_width was given an invalid window");
+        _ =c.pinci_make_error(c.pinc_error_null_handle, "pinc_window_get_width was given an invalid window");
         return 0;
     }
 }
 pub export fn pinc_window_get_height(window: c.pinc_window_incomplete_handle_t) u16 {
     if (window == 0) {
-        _ = pinci_make_error(c.pinc_error_null_handle, "pinc_window_get_height was given a null window");
+        _ =c.pinci_make_error(c.pinc_error_null_handle, "pinc_window_get_height was given a null window");
         return 0;
     }
     const xWindowOrNone = x11_get_x_window(window);
     if (xWindowOrNone) |xWindow| {
         return @intCast(xWindow.height);
     } else {
-        _ = pinci_make_error(c.pinc_error_null_handle, "pinc_window_get_height was given an invalid window");
+        _ =c.pinci_make_error(c.pinc_error_null_handle, "pinc_window_get_height was given an invalid window");
         return 0;
     }
 }
@@ -194,17 +142,17 @@ pub export fn pinc_window_destroy(window: c.pinc_window_incomplete_handle_t) voi
 }
 pub export fn pinc_window_complete(incomplete: c.pinc_window_incomplete_handle_t) c.pinc_window_handle_t {
     if (incomplete == 0) {
-        _ = pinci_make_error(c.pinc_error_null_handle, "pinc_window_complete was given a null handle");
+        _ =c.pinci_make_error(c.pinc_error_null_handle, "pinc_window_complete was given a null handle");
         return 0;
     }
     // Get a pointer to the internal window handle.
     // The list does not change within this function call so the memory will stay valid
     // the internal function does not keep a pointer to the window.
-    const xWindow: *c.x11_window = &Common.windows.items[incomplete - 1].?.native;
+    const xWindow: *c.x11_window = &pinc.windows.items[incomplete - 1].?.native;
     if (!c.x11_window_complete(xWindow)) {
         // completing the window failed, return a null handle.
         // TODO: make error enum more specific
-        _ = pinci_make_error(c.pinc_error_some, "Failed to complete X11 window");
+        _ =c.pinci_make_error(c.pinc_error_some, "Failed to complete X11 window");
         return 0;
     }
     return incomplete;
@@ -213,7 +161,7 @@ pub export fn pinc_window_complete(incomplete: c.pinc_window_incomplete_handle_t
 // pub export fn pinc_window_request_attention(window: c.pinc_window_handle_t) void {}
 // pub export fn pinc_window_close(window: c.pinc_window_handle_t) void {}
 pub export fn pinc_poll_events() void {
-    for (Common.windows.items) |*window| {
+    for (pinc.windows.items) |*window| {
         // TODO: error
         if(window.* == null) return;
         // Some things are backend specific
@@ -227,59 +175,59 @@ pub export fn pinc_poll_events() void {
 
 pub export fn pinc_advance_event() void {
     // I think this stupidity makes the need to refactor event management really obvious
-    _ = Common.pinc_event_union(true);
+    _ = pinc.getEvent(true);
 }
 pub export fn pinc_wait_events(timeout: f32) void {
     c.x11_wait_events(timeout);
     pinc_poll_events();
 }
 pub export fn pinc_event_type() c.pinc_event_type_enum {
-    return Common.pinc_event_union(false).type;
+    return pinc.getEvent(false).type;
 }
 pub export fn pinc_event_window_close_data() c.pinc_event_window_close_t {
-    return Common.pinc_event_union(false).data.window_close;
+    return pinc.getEvent(false).data.window_close;
 }
 pub export fn pinc_event_window_resize_data() c.pinc_event_window_resize_t {
-    return Common.pinc_event_union(false).data.window_resize;
+    return pinc.getEvent(false).data.window_resize;
 }
 pub export fn pinc_event_window_focus_data() c.pinc_event_window_focus_t {
-    return Common.pinc_event_union(false).data.window_focus;
+    return pinc.getEvent(false).data.window_focus;
 }
 pub export fn pinc_event_window_unfocus_data() c.pinc_event_window_unfocus_t {
-    return Common.pinc_event_union(false).data.window_unfocus;
+    return pinc.getEvent(false).data.window_unfocus;
 }
 pub export fn pinc_event_window_damaged_data() c.pinc_event_window_damaged_t {
-    return Common.pinc_event_union(false).data.window_damaged;
+    return pinc.getEvent(false).data.window_damaged;
 }
 pub export fn pinc_event_window_key_down_data() c.pinc_event_window_key_down_t {
-    return Common.pinc_event_union(false).data.window_key_down;
+    return pinc.getEvent(false).data.window_key_down;
 }
 pub export fn pinc_event_window_key_up_data() c.pinc_event_window_key_up_t {
-    return Common.pinc_event_union(false).data.window_key_up;
+    return pinc.getEvent(false).data.window_key_up;
 }
 pub export fn pinc_event_window_key_repeat_data() c.pinc_event_window_key_repeat_t {
-    return Common.pinc_event_union(false).data.window_key_repeat;
+    return pinc.getEvent(false).data.window_key_repeat;
 }
 pub export fn pinc_event_window_text_data() c.pinc_event_window_text_t {
-    return Common.pinc_event_union(false).data.window_text;
+    return pinc.getEvent(false).data.window_text;
 }
 pub export fn pinc_event_window_cursor_move_data() c.pinc_event_window_cursor_move_t {
-    return Common.pinc_event_union(false).data.window_cursor_move;
+    return pinc.getEvent(false).data.window_cursor_move;
 }
 pub export fn pinc_event_window_cursor_enter_data() c.pinc_event_window_cursor_enter_t {
-    return Common.pinc_event_union(false).data.window_cursor_enter;
+    return pinc.getEvent(false).data.window_cursor_enter;
 }
 pub export fn pinc_event_window_cursor_exit_data() c.pinc_event_window_cursor_exit_t {
-    return Common.pinc_event_union(false).data.window_cursor_exit;
+    return pinc.getEvent(false).data.window_cursor_exit;
 }
 pub export fn pinc_event_window_cursor_button_down_data() c.pinc_event_window_cursor_button_down_t {
-    return Common.pinc_event_union(false).data.window_cursor_button_down;
+    return pinc.getEvent(false).data.window_cursor_button_down;
 }
 pub export fn pinc_event_window_cursor_button_up_data() c.pinc_event_window_cursor_button_up_t {
-    return Common.pinc_event_union(false).data.window_cursor_button_up;
+    return pinc.getEvent(false).data.window_cursor_button_up;
 }
 pub export fn pinc_event_window_scroll_data() c.pinc_event_window_scroll_t {
-    return Common.pinc_event_union(false).data.window_scroll;
+    return pinc.getEvent(false).data.window_scroll;
 }
 pub export fn pinc_key_name(code: c.pinc_key_code_enum) [*:0]const u8 {
     switch (code) {
