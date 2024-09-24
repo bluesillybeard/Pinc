0d182468726e8327f154f63c1dc81c220061b6bc
bluesillybeard <bluesillybeard2@gmail.com>
2024-09-23 21:24:32 -0600

    Add opengl to zig "binding"

--- include/pinc.zig
+++ include/pinc.zig
@@ -1,3 +1,6 @@
 // This is so Pinc can be used as a regular dependency as part of a Zig project
 // without needing to do some kind of hack to get the header
-pub usingnamespace @cImport(@cInclude("pinc.h"));
+pub usingnamespace @cImport({
+    @cDefine("PINC_RAW_OPENGL", 1);
+    @cInclude("pinc.h");
+});
